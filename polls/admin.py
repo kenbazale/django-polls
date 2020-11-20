8ce7e244814c1e36dccc414366eed019bf9cf0fb
kenbazale <kenbazale@livie.com>
2020-11-20 13:24:21 +0200

    polls
    django polls app

---
+++ polls/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import Question,Choice
+
+# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+
+
+admin.site.register(Question,QuestionAdmin)
